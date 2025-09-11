278475dd63220cdc1a78e5e479c3fa06ec899bbd
Ian Lewis <33504743+wellthathappened@users.noreply.github.com>
2025-09-11 10:42:02 -0700

    Added camera movement and rotation (FPS)

--- Lewis_Unity_AM/Assets/Scripts/PlayerController.cs
+++ Lewis_Unity_AM/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    Vector2 cameraRotation;
+    Vector3 cameraOffset;
+    InputAction lookVector;
+    Camera playerCam;
+
     Rigidbody rb;
 
     float verticalMove;
@@ -11,14 +16,34 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 5f;
     public float jumpHeight = 10f;
+    public float Xsensitivty = 1.0f;
+    public float Ysensitivty = 1.0f;
+    public float camRotationLimit = 90.0f;
 
     public void Start()
     {
+        cameraOffset = new Vector3(0, .5f, .5f);
         rb = GetComponent<Rigidbody>();
+        playerCam = Camera.main;
+        lookVector = GetComponent<PlayerInput>().currentActionMap.FindAction("Look");
+        cameraRotation = Vector2.zero;
     }
 
     private void Update()
     {
+        // Camera Rotation System
+        playerCam.transform.position = transform.position + cameraOffset;
+
+        cameraRotation.x += lookVector.ReadValue<Vector2>().x * Xsensitivty;
+        cameraRotation.y += lookVector.ReadValue<Vector2>().y * Ysensitivty;
+
+        cameraRotation.y = Mathf.Clamp(cameraRotation.y, -camRotationLimit, camRotationLimit);
+
+        playerCam.transform.rotation = Quaternion.Euler(-cameraRotation.y, cameraRotation.x, 0);
+        transform.localRotation = Quaternion.AngleAxis(cameraRotation.x, Vector3.up);
+        
+
+        // Movement System
         Vector3 temp = rb.linearVelocity;
 
         temp.x = verticalMove * speed;
