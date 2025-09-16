cf2ea0d486a552c861663d863696da27a8dea8be
Ian Lewis <33504743+wellthathappened@users.noreply.github.com>
2025-09-16 13:49:35 -0700

    Added Jump

--- Lewis_Unity_AM/Assets/Scripts/PlayerController.cs
+++ Lewis_Unity_AM/Assets/Scripts/PlayerController.cs
@@ -4,20 +4,17 @@ using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    Vector2 cameraRotation;
-    InputAction lookVector;
-    Transform playerCam;
+    Camera playerCam;
 
     Rigidbody rb;
+    Ray ray;
 
     float verticalMove;
     float horizontalMove;
 
     public float speed = 5f;
     public float jumpHeight = 10f;
-    public float Xsensitivty = 1.0f;
-    public float Ysensitivty = 1.0f;
-    public float camRotationLimit = 90.0f;
+    public float groundDetectLength = .5f;
 
     public int health = 5;
     public int maxHealth = 5;
@@ -25,10 +22,9 @@ public class PlayerController : MonoBehaviour
 
     public void Start()
     {
+        ray = new Ray(transform.position, -transform.up);
         rb = GetComponent<Rigidbody>();
-        playerCam = transform.GetChild(0);
-        lookVector = GetComponent<PlayerInput>().currentActionMap.FindAction("Look");
-        cameraRotation = Vector2.zero;
+        playerCam = Camera.main;
 
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -41,13 +37,10 @@ public class PlayerController : MonoBehaviour
 
         
         // Camera Rotation System
-        cameraRotation.x += lookVector.ReadValue<Vector2>().x * Xsensitivty;
-        cameraRotation.y += lookVector.ReadValue<Vector2>().y * Ysensitivty;
-
-        cameraRotation.y = Mathf.Clamp(cameraRotation.y, -camRotationLimit, camRotationLimit);
-
-        playerCam.rotation = Quaternion.Euler(-cameraRotation.y, cameraRotation.x, 0);
-        transform.localRotation = Quaternion.AngleAxis(cameraRotation.x, Vector3.up);
+        Quaternion playerRotation = playerCam.transform.rotation;
+        playerRotation.x = 0;
+        playerRotation.z = 0;
+        transform.rotation = playerRotation;
         
 
         // Movement System
@@ -55,6 +48,11 @@ public class PlayerController : MonoBehaviour
 
         temp.x = verticalMove * speed;
         temp.z = horizontalMove * speed;
+
+        ray.origin = transform.position;
+        ray.direction = -transform.up;
+
+        
 
         rb.linearVelocity = (temp.x * transform.forward) + 
                             (temp.y * transform.up) + 
@@ -67,6 +65,12 @@ public class PlayerController : MonoBehaviour
 
         verticalMove = inputAxis.y;
         horizontalMove = inputAxis.x;
+    }
+
+    public void Jump()
+    {
+        if (Physics.Raycast(ray, groundDetectLength))
+            rb.AddForce(transform.up * jumpHeight, ForceMode.Impulse);
     }
     private void OnTriggerEnter(Collider other)
     {
