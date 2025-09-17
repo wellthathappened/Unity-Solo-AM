7622394b6d9f52b9d4e4321e1c5c0703034fc4ce
Ian Lewis <33504743+wellthathappened@users.noreply.github.com>
2025-09-17 10:24:59 -0700

    Started Enemy Setup

--- Lewis_Unity_AM/Assets/Scripts/PlayerController.cs
+++ Lewis_Unity_AM/Assets/Scripts/PlayerController.cs
@@ -36,14 +36,14 @@ public class PlayerController : MonoBehaviour
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
         
-        // Camera Rotation System
+        // Player Rotation (Horiztonally)
         Quaternion playerRotation = playerCam.transform.rotation;
         playerRotation.x = 0;
         playerRotation.z = 0;
         transform.rotation = playerRotation;
         
 
-        // Movement System
+        // Movement System (Take vert/horiz input and convert to 3D movement)
         Vector3 temp = rb.linearVelocity;
 
         temp.x = verticalMove * speed;
@@ -58,7 +58,7 @@ public class PlayerController : MonoBehaviour
                             (temp.y * transform.up) + 
                             (temp.z * transform.right);
     }
-
+    // Read player input and convert to values to be used by rb for movement
     public void Move(InputAction.CallbackContext context)
     {
         Vector2 inputAxis = context.ReadValue<Vector2>();
@@ -66,7 +66,7 @@ public class PlayerController : MonoBehaviour
         verticalMove = inputAxis.y;
         horizontalMove = inputAxis.x;
     }
-
+    // If raycast downward sees collider, player can jump.
     public void Jump()
     {
         if (Physics.Raycast(ray, groundDetectLength))
@@ -76,5 +76,16 @@ public class PlayerController : MonoBehaviour
     {
         if (other.tag == "killzone")
             health = 0;
+
+        if ((other.tag == "health") && (health < maxHealth))
+        {
+            health++;
+            Destroy(other.gameObject);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "hazard")
+            health--;
     }
 }
